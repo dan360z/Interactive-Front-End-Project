b10aceb89e00fd947e4cde76e09a6fc52a5c96bf
Daniel Field <dannyf506@googlemail.com>
2018-12-13 19:12:05 +0000

    Added search

--- assets/js/map.js
+++ assets/js/map.js
@@ -10,4 +10,21 @@ function initMap() {
             lng: -0.1278
         }
     });
+    //-------------------Place search---------------------------------------
+
+    var searchBox = new google.maps.places.SearchBox(document.getElementById("searchPlace"));
+
+    //place change event on searchInput
+    google.maps.event.addListener(searchBox, "places_changed", function() {
+        var places = searchBox.getPlaces();
+        //Bounds
+        var bounds = new google.maps.LatLngBounds();
+        var i, place;
+        for (i = 0; place = places[i]; i++) {
+            bounds.extend(place.geometry.location);
+        }
+        //Fit to the bound
+        map.fitBounds(bounds);
+        map.setZoom(14);
+    });
 }

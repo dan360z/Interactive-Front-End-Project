cbb967e56379c744997fd84490750274e874255d
Daniel Field <dannyf506@googlemail.com>
2019-01-04 23:42:32 +0000

    added a tap on map instruction

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
 
    $('.search-container').hide();
    $('.menu').hide();
-
+   $('#tapMap').hide();
+   //--------------------------------------------------------------
    $('#search-btn').click(function() {
       $('.menu').slideUp();
       $('.search-container').slideToggle();
@@ -12,7 +13,17 @@ $(document).ready(function() {
       $('.search-container').slideUp();
       $('.menu').slideToggle();
    });
+   //-------------------------------------------------------------------
+   
+   $('.place-type').children().click(function() {
+      $('#tapMap').show();
+      setTimeout(function() {
+         $('#tapMap').hide();
+      }, 2500);
+   });
 
+   //----------------------------------------------------------
+   
    $('#accomodation').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');

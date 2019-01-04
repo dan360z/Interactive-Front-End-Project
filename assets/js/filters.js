a0af982aafc6054ed02658fc059cbab61433d3c2
Daniel Field <dannyf506@googlemail.com>
2019-01-04 23:50:14 +0000

    simplified filters.js and reduced setTimeout function time on places buttons click hide menu

--- assets/js/filters.js
+++ assets/js/filters.js
@@ -21,134 +21,88 @@ $(document).ready(function() {
          $('#tapMap').hide();
       }, 2500);
    });
+   
+     //-------------------------------------------------------------------
+   
+   $('.place-type').children().click(function() {
+      setTimeout(function() {
+         $('.menu').slideToggle();
+      }, 150);
+   });
 
    //----------------------------------------------------------
    
    $('#accomodation').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#atm').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#store').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#bar').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#restaurant').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#night-club').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#casino').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#bowling').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#shopping').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#park').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#amusement-park').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#museum').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#aquarium').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#zoo').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#train-station').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#taxi-stand').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#transit-station').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
    $('#subway-station').click(function() {
       $('button').removeClass('selected');
       $(this).addClass('selected');
-      setTimeout(function() {
-         $('.menu').slideToggle();
-      }, 200);
    });
 
 });
